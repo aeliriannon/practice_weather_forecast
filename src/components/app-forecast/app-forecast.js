738b5e1741822f057bacc771852bb07dd7853c34
Glaz <i.glaz@itransition.com>
2022-04-14 02:02:21 +0400

    added get request for one city and added data-content for oneDay forecast

--- src/components/app-forecast/app-forecast.js
+++ src/components/app-forecast/app-forecast.js
@@ -1,10 +1,11 @@
+
 import AppForecastTabs from '../app-forecast-tabs/app-forecast-tabs';
 import AppForecastTabsOne from '../app-frecast-tabs-one/app-forecast-tabs-one';
 import AppForecastTabsMore from '../app-forecast-tabs-more/app-forecast-tabs-more';
 
 import './app-forecast.css';
 
-const AppForecast = ({ temp, condition, conditionClass }) => {
+const AppForecast = ({ condition, conditionClass }) => {
 
     const dataForecast = [
         { temperature: '12', condition: 'beach_access', conditionClass: 'material-icons  _rain' },
@@ -20,7 +21,7 @@ const AppForecast = ({ temp, condition, conditionClass }) => {
                 <div className="card app-forecast">
                     <AppForecastTabs />
                     <div className="card-content blue-grey lighten-2">
-                        <AppForecastTabsOne temp={22} condition='beach_access' />
+                        <AppForecastTabsOne />
                         <AppForecastTabsMore dataForecast={dataForecast} />
                     </div>
                 </div>
--- src/components/app-frecast-tabs-one/app-forecast-tabs-one.js
+++ src/components/app-frecast-tabs-one/app-forecast-tabs-one.js
@@ -1,31 +1,43 @@
+import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './app-forecast-tabs-one.css';
 
-const AppForecastTabsOne = ({ temp, condition, conditionClass }) => {
-    if (condition === 'ac_unit') {
-        conditionClass = 'material-icons _snow';
-    } else if (condition === 'wb_sunny') {
-        conditionClass = 'material-icons _sun';
-    } else if (condition === 'cloud_queue') {
-        conditionClass = 'material-icons _cloud';
-    } else if (condition === 'beach_access') {
-        conditionClass = 'material-icons _rain';
-    }
+
+
+const AppForecastTabsOne = () => {
+
+    let oneForecast = [];
+    axios.get('https://api.openweathermap.org/data/2.5/weather?q=Минск,BY&units=metric&lang=ru&appid=856663d2b72927fe7a6f140d38a378a3')
+        .then(data => {
+            oneForecast.push(data.data.main, data.data.weather[0]);
+            let temperature = oneForecast[0].temp;
+            let description = oneForecast[1].description;
+            ReactDOM.render(
+                <div className="row">
+                    <div className="col s6">
+                        <h2>{temperature}°C</h2>
+                    </div>
+                    <div className="col s6">
+                        <p className='right-align _description'>{description}</p>
+                    </div>
+
+                </div>, document.getElementById('oneForecast')
+            )
+
+        });
+
     return (
         <div id="one" className='app-forecast__one'>
             <div className="row valign-wrapper">
-                <div className="col s12">
-                    <div className="row">
-                        <div className="col s6">
-                            <h2>{temp}°C</h2>
-                        </div>
-                        <div className="col s6">
-                            <p className='right-align'><i className={conditionClass}>{condition}</i></p>
-                        </div>
-                    </div>
+                <div className="col s12" id='oneForecast'>
+
                 </div>
             </div>
         </div>
-    );
+    )
+
 }
 
 export default AppForecastTabsOne;
